479ea4b7ddba164182c066365b97d81738aa68dd
WeiGuo <weig@microsoft.com>
2017-12-20 01:26:31 -0800

    added EDA utilities

--- Utilities.py
+++ Utilities.py
@@ -5,31 +5,23 @@ import matplotlib.pyplot as plt
 
 # create summary table for numerical and categorical features
 def create_summary_table(df):
-   
     def num_missing(x):
-        return len(x.index)-x.count()
-    
+        return len(x.index)-x.count()  
     def pct_missing(x):
         return (len(x.index)-x.count())/x.count()
-
     def num_unique(x):
         return len(x.value_counts(dropna=False))
 
     # for numerical features
     temp_df = df.describe().T
-
     missing_df = pd.DataFrame(df.apply(num_missing, axis=0)) 
     missing_df.columns = ['missing']
-
     pct_missing_df = pd.DataFrame(df.apply(pct_missing, axis=0))
     pct_missing_df.columns = ['pct_missing']
-    
     unq_df = pd.DataFrame(df.apply(num_unique, axis=0))
     unq_df.columns = ['unique']
-
     types_df = pd.DataFrame(df.dtypes)
     types_df.columns = ['DataType']   
-
     summary_df_cont = temp_df.join(missing_df).join(pct_missing_df).join(unq_df).join(types_df)
     
     # for Cat features
@@ -37,14 +29,11 @@ def create_summary_table(df):
     num_cols = len(col_names)
     index = range(num_cols)
     cat_index = []
-
     for i in index:
         if df.dtypes[i] == 'object':
             cat_index.append(i)
-
     summary_df_cat = missing_df.join(unq_df).join(types_df.iloc[cat_index], how='inner')
     summary_df_cat
-
     return summary_df_cont, summary_df_cat
 
 
@@ -63,12 +52,9 @@ def create_time_feature(df):
     #create created_at_year, created_at_month, created_at_day, created_at_date, created_at_dayOfWeek, 
     #created_at_time, created_at_hour, created_at_minute, created_at_second, created_at_isWeekend,
     #created_at_isHoliday
-
     df['created_at_year'], df['created_at_month'], df['created_at_day'], df['created_at_date'], df['created_at_dayOfWeek'], df['created_at_time'], df['created_at_hour'], df['created_at_minute'], df['created_at_second'] = df['created_at_datetime'].dt.year, df['created_at_datetime'].dt.month, df['created_at_datetime'].dt.day, df['created_at_datetime'].dt.date, df['created_at_datetime'].dt.dayofweek, df['created_at_datetime'].dt.time, df['created_at_datetime'].dt.hour, df['created_at_datetime'].dt.minute, df['created_at_datetime'].dt.second
-
     df.loc[df['created_at_dayOfWeek'].isin([5, 6]), 'created_at_isWeekend'] = 1
     df.loc[df['created_at_dayOfWeek'].isin([0, 1, 2, 3, 4]), 'created_at_isWeekend'] = 0
-
     cal = calendar()
     holidays = cal.holidays(start=df['created_at_date'].min(), end=df['created_at_date'].max())
     df['created_at_isHoliday'] = np.where(df.created_at_datetime.dt.normalize().isin(holidays), 1, 0)
